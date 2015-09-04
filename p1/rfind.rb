a5ba36376b17909bbf41641b9fa25309b9c28b04
Jonathan Allen <jallen59@mail.csuchico.edu>
2015-09-03 23:56:44 -0700

    down to 9 lines

--- p1/rfind.rb
+++ p1/rfind.rb
@@ -2,8 +2,7 @@
 files = `find .`.split.sort.keep_if {|arg| arg =~ /(\.(rb|erb|js|css|html|yml|txt))$/}
 puts "Files with names that matches <#{ARGV[0]}>"
 files.each {|arg| if arg =~ /#{ARGV[0]}/ then puts "  #{arg}" end}
-puts "**************************************************"
-puts "Files with content that matches <#{ARGV[0]}>"
+puts "**************************************************\nFiles with content that matches <#{ARGV[0]}>"
 titles = files.map {|arg| if `grep -i -n #{ARGV[0]} #{arg}`.length > 1 then arg end}.compact
 titles.each {|arg| puts arg
              a = `grep -i -n #{ARGV[0]} #{arg}`.split("\n")
