48b0387aff0501190ce1e13b35c3d35ae2880f3f
Jonathan Allen <jallen59@mail.csuchico.edu>
2015-09-04 22:06:54 -0700

    test

--- p1/tfind.rb
+++ p1/tfind.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
-puts "Files with names that matches <#{ARGV[0]}>"
-(files = `find .`.split.sort.keep_if {|x| x =~ /(\.(rb|erb|js|css|html|yml|txt))$/}.keep_if {|x| `grep -i -n #{ARGV[0]} #{x}`.length > 0}).each {|x| if x =~ /#{ARGV[0]}/ then puts "  #{x}" end}
+puts "Files with names that matches <#{ARGV[0]}>"if(a = `find .`.split.sort.keep_if {|x| x =~ /(\.(rb|erb|js|css|html|yml|txt))$/}.keep_if {|x| `grep -i -n #{ARGV[0]} #{x}`.length > 0}).each {|x| puts "  #{x}" if x =~ /#{ARGV[0]}/}
 puts "*"*50+"\nFiles with content that matches <#{ARGV[0]}>"
-files.each {|x| puts x
-             (a = `grep -i -n #{ARGV[0]} #{x}`.split("\n")).each {|x| puts "  #{x}"}
-             puts "-"*50 if x != files.last}
+a.each {|x| puts x
+         (a = `grep -i -n #{ARGV[0]} #{x}`.split("\n")).each {|x| puts "  #{x}"}
+         puts "-"*50 if x != a.last}
