4d0380c098bf6451818b67f0957853f21d673561
Jonathan Allen <jallen59@mail.csuchico.edu>
2015-09-16 17:43:14 -0700

    test

--- p1/tfind.rb
+++ p1/tfind.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
-puts "Files with names that matches <#{ARGV[0]}>"if(a = `find .`.split.sort.keep_if {|x| x =~ /(\.(rb|erb|js|css|html|yml|txt))$/}.keep_if {|x| `grep -i -n #{ARGV[0]} #{x}`.length > 0}).each {|x| puts "  #{x}" if x =~ /#{ARGV[0]}/}
+puts "Files with names that matches <#{ARGV[0]}>"
+((a = `find .`.split.sort.keep_if {|x| x =~ /(\.(rb|erb|js|css|html|yml|txt))$/}).keep_if {|x| `grep -i -n #{ARGV[0]} #{x}`.length > 1}).each {|x| puts "  #{x}" if x =~ /#{ARGV[0]}/}
 puts "*"*50+"\nFiles with content that matches <#{ARGV[0]}>"
 a.each {|x| puts x
          (a = `grep -i -n #{ARGV[0]} #{x}`.split("\n")).each {|x| puts "  #{x}"}
