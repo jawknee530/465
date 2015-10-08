9d009a2acdbcaa6abe6ce79c6cbe538f70af6724
Jonathan Allen <jallen59@mail.csuchico.edu>
2015-10-07 18:26:44 -0700

    almost done

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -3,9 +3,28 @@ class HomeController < ApplicationController
 load "#{Rails.root}/lib/ladder.rb"
 
 def index
+  @word1 = dictionary.sample
+  @word2 = dictionary.sample
+  while @word1 == @word2 do
+    @word1 = dictionary.sample
+    @word2 = dictionary.sample
+  end
 end
 
 def show
+  @start = params[:start_word]
+  @word1 = params[:word1]
+  @word2 = params[:word2]
+  @word3 = params[:word3]
+  @word4 = params[:word4]
+  @word5 = params[:word5]
+  @end = params[:end_word]
+  if legal [@start, @word1, @word2, @word3, @word4, @word5, @end] then
+    @result = "Congrats you won"
+  else
+    @result = "Sorry you lose"
+  end
+
 end
 
 end
