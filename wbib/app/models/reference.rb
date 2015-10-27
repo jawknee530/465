3e29ff1bdf315a98cb372a486d14a861911f591f
Jonathan Allen <jallen59@mail.csuchico.edu>
2015-10-26 20:16:43 -0700

    got the url crap working

--- wbib/app/models/reference.rb
+++ wbib/app/models/reference.rb
@@ -1,3 +1,13 @@
 class Reference < ActiveRecord::Base
+  require 'uri'
+  validates :URL, presence: true
+  before_save :checkurl
   belongs_to :topic
+end
+
+def checkurl
+  uri = URI.parse(self.URL)
+  if uri !=~ /\A#{URI::regexp(['http', 'https'])}\z/
+    self.URL = "http://#{self.URL}"
+  end
 end
